c786f1f2fb3b5734d93cdac9ad8561e701ec88e2
adney002 <a.tran5975@edmail.edcc.edu>
2022-04-03 21:03:43 -0700

    resolved all same test case

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { ROLL_TYPES } from "./constants";
+
 const main = () => {
   console.log("hi mom");
 };
@@ -8,4 +10,27 @@ main();
  * @param {number[]} dice - array of numbers representing dice
  * @returns {string} the highest roll type
  */
-export const findBestRollType = (dice) => {};
+export const findBestRollType = (dice) => {
+  const diceCount = initializeCounts(dice);
+  if (diceCount.size === 1) {
+    return ROLL_TYPES.allSame;
+  }
+};
+
+/**
+ * @param {number[]} dice - array of numbers representing dice
+ * @returns {Map<number, number>} a map that stores the number of occurances of each die value
+ */
+const initializeCounts = (dice) => {
+  const diceCount = new Map();
+
+  dice.forEach((die) => {
+    const count = diceCount.get(die);
+    if (!count) {
+      diceCount.set(die, 1);
+    } else {
+      diceCount.set(die, count + 1);
+    }
+  });
+  return diceCount;
+};

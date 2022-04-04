00f495b890c3ae5c18df819f9e91aeee666f0b45
adney002 <a.tran5975@edmail.edcc.edu>
2022-04-03 21:25:50 -0700

    added large straight test cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,23 @@
 import { findBestRollType } from "../src/index";
 import { ROLL_TYPES } from "../src/constants";
+import shuffle from "../src/utils/shuffle";
 
 describe("Determining roll types", () => {
+  let dice;
+
   it("should have an 'all same' type if all the dice are the same number", () => {
-    const dice = [1, 1, 1, 1, 1];
-    expect(findBestRollType(dice)).toEqual(ROLL_TYPES.allSame);
+    dice = [1, 1, 1, 1, 1];
+    expect(findBestRollType(shuffle(dice))).toEqual(ROLL_TYPES.allSame);
+  });
+
+  it("should be a straight if there are five consecutive numbers", () => {
+    dice = [1, 2, 3, 4, 5];
+    expect(findBestRollType(shuffle(dice))).toEqual(ROLL_TYPES.straight);
+
+    dice = [2, 3, 4, 5, 6];
+    expect(findBestRollType(shuffle(dice))).toEqual(ROLL_TYPES.straight);
+
+    dice = [1, 2, 3, 5, 6];
+    expect(findBestRollType(shuffle(dice))).not.toEqual(ROLL_TYPES.straight);
   });
 });
